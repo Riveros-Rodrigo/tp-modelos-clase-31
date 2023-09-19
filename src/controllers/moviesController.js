64bffe1ddb70c756f48bc5e1541a7b4d3489b65d
Rodrigo Riveros <riverosdev@gmail.com>
2023-09-19 15:01:46 -0300

    agrego fuunciones de movies

--- src/controllers/moviesController.js
+++ src/controllers/moviesController.js
@@ -11,13 +11,41 @@ module.exports = {
             })
             .catch((error)=> console.log(error))
     },
+    detail: (req,res) =>{
+        const {id} = req.params;
+        db.Movie.findByPk(id) //findByPk trae por la clave primaria
+            .then(movie => { //promesa que devuelve una pelicula
+                return res.render('moviesDetail',{
+                    movie
+                })
+            })
+            .catch(error => console.log(error))
+    },
     new : (req,res) =>{
-        return res.send('Lista de estreno')
+        db.Movie.findAll({ //el findAll siempre devuelve un array
+            order : [ //aca le doy la consulta del order by en lista descendiente
+                ['release_date','DESC']
+            ]
+        })
+        .then((movies) =>{
+            return res.render('newestMovies',{
+                movies
+            })
+        })
+        .catch((error)=> console.log(error))
     },
     recomended: (req,res) =>{
-        return res.send('Lista de recomendados')
+        db.Movie.findAll({ 
+            limit : 5, //la consulta se genera bien pero aca no importa ponerlo en orden
+            order : [
+                ['rating', 'DESC']
+            ]
+        })
+        .then((movies) =>{
+            return res.render('recommendedMovies',{
+                movies
+            })
+        })
+        .catch((error)=> console.log(error))
     },
-    detail: (req,res) =>{
-        return res.send('Lista de detalles')
-    }
 }
